b6179f2bfe214324c2e2d6caa756bb6e1db50a5d
ALEXANDRE SATOCHI YAMAMOTO <108668607+satochi-yamamoto@users.noreply.github.com>
2024-07-11 14:59:49 -0300

    Update enviar-mensagem.php

--- enviar-mensagem.php
+++ enviar-mensagem.php
@@ -1,12 +1,14 @@
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $nome = htmlspecialchars($_POST['nome']);
+    $sobrenome = htmlspecialchars($_POST['sobrenome']);
     $email = htmlspecialchars($_POST['email']);
+    $assunto = htmlspecialchars($_POST['assunto']);
     $mensagem = htmlspecialchars($_POST['mensagem']);
     
-    $to = "seu-email@example.com";
-    $subject = "Mensagem do Site";
-    $body = "Nome: $nome\nEmail: $email\nMensagem:\n$mensagem";
+    $to = "info@meusite.com";
+    $subject = "Contato do site: $assunto";
+    $body = "Nome: $nome $sobrenome\nEmail: $email\nMensagem:\n$mensagem";
     $headers = "From: $email";
 
     if (mail($to, $subject, $body, $headers)) {
